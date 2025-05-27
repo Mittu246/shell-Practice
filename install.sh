33b5f2a20e566751a2a67df3f139ca670191af39
Mittu246 <srikeshmla@gmail.com>
2025-05-27 22:04:39 +1000

    condition if

--- install.sh
+++ install.sh
@@ -10,18 +10,43 @@ else
     echo "running with root access"
 fi
 
-dnf list installed mysql
+VALIDATE(){
+    if [ $1 -eq 0 ]
+then
+   echo "my $2 install is success"
+else
+   echo "my $2 install is not sucess"
+   exit 1
+fi
+
+}
+dnf list installed mysql 
+# check already installed or not .if installed $? is 0, then
+# if $? is 1 else
 if [ $? -ne 0 ]
 then
    echo "mysql is going to install "
    dnf install mysql -y
-   if [ $? -eq 0 ]
-then
-   echo "my sql install is success"
-else
-   echo "my sql install is not sucess"
-   exit 1
-fi
+   VALIDATE $? "sql"
 else
    echo "my sql is already insatlled..nothing to do it"
+fi
+
+dnf list installed nginx
+if [ $? -ne 0 ]
+then
+   echo "nginx is going to intsall"
+   dnf install nginx -y
+   VALIDATE $? "nginx"
+else
+   echo "nginx is alreasdy installed...nothing to do it"
+fi
+dnf list installed phython3
+if [ $? -ne 0 ] 
+then
+   echo "phython3 is going to install"
+   dnf install phython3 -y
+   VALIDATE $? "phython3"
+else
+    echo "phython3 is already installed
 fi
